c3a65cf6cef013101151b8452cea2e08dee1c5a5
Miguel Angel Gonzales Melgar <miguelangel.gonzales@assuresoft.tech>
2025-05-25 22:49:32 -0400

    wip: create ColorSwatches component and add props and classes.

--- src/components/color-picker/ColorSwatches.tsx
+++ src/components/color-picker/ColorSwatches.tsx
@@ -1,8 +1,30 @@
-const ColorSwatches = () => {
+interface ColorSwatchesProps {
+	colors: string[];
+	backgroundColor: string;
+	onColorChange: (color: string) => void;
+}
+
+const ColorSwatches = ({
+	colors,
+	backgroundColor,
+	onColorChange,
+}: ColorSwatchesProps) => {
 	return (
 		<section>
 			<h3>Available colors:</h3>
-			<div>color</div>
+			<div className="colors-container">
+				{colors.map((color) => (
+					<div
+						className="colors"
+						key={color}
+						onClick={() => onColorChange(color)}
+						style={{ backgroundColor: color }}
+					>
+						{color}
+					</div>
+				))}
+			</div>
+			<p>Current color background is: {backgroundColor}</p>
 		</section>
 	);
 };
