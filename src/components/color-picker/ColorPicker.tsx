1a20eed3c4bbb5c6cc2459746c9dd4c289f61e01
Miguel Angel Gonzales Melgar <miguelangel.gonzales@assuresoft.tech>
2025-05-25 22:22:35 -0400

    feature: add state to ColorPicker component that handles input value and onclick event in button.

--- src/components/color-picker/ColorPicker.tsx
+++ src/components/color-picker/ColorPicker.tsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import ColorSwatches from "./ColorSwatches";
 
 const ColorPicker = () => {
+	// State for the input value
+	const [inputValue, setInputValue] = useState<string>("");
+
+	// state for adding colors
+	const [colors, setColors] = useState<string[]>([
+		"#ff5733",
+		"#FFC300",
+		"#581845",
+	]);
+
+	// handlers
+	const handleInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInputValue(event.target.value);
+	};
+
+	const handleAddColor = () => {
+		if (!colors.includes(inputValue)) {
+			setColors([...colors, inputValue]);
+			setInputValue("");
+		} else {
+			alert("This color is already in the list.");
+		}
+	};
 	return (
 		<main>
 			<h3>Wellcome! please select a background color:</h3>
@@ -9,8 +33,10 @@ const ColorPicker = () => {
 			<input
 				type="text"
 				placeholder="Hex color: e.g. #ff000"
+				value={inputValue}
+				onChange={handleInputValue}
 			/>
-			<button>Add new color</button>
+			<button onClick={handleAddColor}>Add new color</button>
 			<ColorSwatches />
 		</main>
 	);
